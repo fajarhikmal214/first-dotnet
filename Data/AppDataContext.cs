07f0b04b28ca1e930aeb47f91fe82e8fcfd276d4
Fajar Hikmal <fajarhikmal214@gmail.com>
2025-05-07 17:05:44 +0700

    migrations: add category table

--- Data/AppDataContext.cs
+++ Data/AppDataContext.cs
@@ -8,5 +8,6 @@ namespace HelloWorld.Data
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
 
         public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
     }
 }
---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+namespace HelloWorld.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
